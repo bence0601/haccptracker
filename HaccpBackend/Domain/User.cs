a59877cb68f25a6587375a4c5728143b98a0726a
Bence Nagy <nagybence0601@gmail.com>
2026-04-10 14:49:50 +0200

    feat(backend): add properties for user entity

--- HaccpBackend/Domain/User.cs
+++ HaccpBackend/Domain/User.cs
@@ -2,5 +2,18 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public int or
+                
+
+
+
+
+
     }
 }
