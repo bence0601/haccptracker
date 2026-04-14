915239a17667c91f6244c4500801f65ab831e13f
Bence Nagy <nagybence0601@gmail.com>
2026-04-14 15:49:29 +0200

    feat(backend): implement properties of vendor entity

--- HaccpBackend/Domain/Organization.cs
+++ HaccpBackend/Domain/Organization.cs
@@ -4,18 +4,14 @@ namespace HaccpBackend.Domain
 {
     public class Organization : IAuditableEntity
     {
-
-
         public int Id { get; init; }
         public required string Name { get; set; }
-        public required string PhoneNumber { get; set; }
-
+        public string? PhoneNumber { get; set; }
         public DateTime CreatedOnUtc { get; private set; }
         public DateTime? ModifiedOnUtc { get; private set; }
 
         public Organization()
         {
         }
-
     }
 }
--- HaccpBackend/Domain/Vendor.cs
+++ HaccpBackend/Domain/Vendor.cs
@@ -1,6 +1,19 @@
-﻿namespace HaccpBackend.Domain
+﻿using HaccpBackend.Common;
+
+namespace HaccpBackend.Domain
 {
-    public class Vendor
+    public class Vendor : IAuditableEntity
     {
+        public int Id { get; init; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public DateTime CreatedOnUtc { get; private set; }
+        public DateTime? ModifiedOnUtc { get; private set; }
+
+        public Vendor()
+        {
+        }
     }
 }
