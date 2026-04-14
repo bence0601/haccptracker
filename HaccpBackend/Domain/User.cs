e83003cb32a526557f86019cc0a8f1406c9c1648
Bence Nagy <nagybence0601@gmail.com>
2026-04-14 12:16:35 +0200

    feat(backend): add interceptor for audit properties

--- HaccpBackend/Domain/User.cs
+++ HaccpBackend/Domain/User.cs
@@ -1,19 +1,18 @@
-﻿namespace HaccpBackend.Domain
+﻿using HaccpBackend.Common;
+
+namespace HaccpBackend.Domain
 {
-    public class User
+    public class User : IAuditableEntity
     {
-        public int Id { get; set; }
+        public int Id { get; init; }
         public required string UserName { get; set; }
         public required string PasswordHash { get; set; }
         public required string Email { get; set; }
         public string? PhoneNumber { get; set; }
 
-        public int or
-                
+        public Organization? Organization { get; set; }
 
-
-
-
-
+        public DateTime CreatedOnUtc { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? ModifiedOnUtc { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
--- HaccpBackend/Program.cs
+++ HaccpBackend/Program.cs
@@ -1,3 +1,7 @@
+using HaccpBackend.Interceptor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<UpdateAuditableEntitiesInterceptor>();
+
+//builder.Services.AddDbContext<ApplicationDbContext>((sp, optionsBuilder) =>
+//{
+//    var auditableInterceptor = sp.GetService<UpdateAuditableEntitiesInterceptor>();
+
+//    optionsBuilder.UseNpgsql(connectionString,)
+//})
 
 var app = builder.Build();
 
