4e699e33619962165b350753568c787616f21030
bence0601 <nagybence0601@gmail.com>
2026-04-21 14:29:22 +0200

    feat(backend): add inheritance from IdentityUser to User class, remove redundant properties

--- HaccpBackend/Domain/Users/User.cs
+++ HaccpBackend/Domain/Users/User.cs
@@ -1,16 +1,11 @@
 ﻿using HaccpBackend.Common;
 using HaccpBackend.Domain.Organizations;
+using Microsoft.AspNetCore.Identity;
 
 namespace HaccpBackend.Domain.Users
 {
-    public class User : IAuditableEntity
+    public class User : IdentityUser<int>, IAuditableEntity
     {
-        public int Id { get; init; }
-        public required string UserName { get; set; }
-        public required string PasswordHash { get; set; }
-        public required string Email { get; set; }
-        public string? PhoneNumber { get; set; }
-
         public required Organization Organization { get; set; }
 
         public DateTime CreatedOnUtc { get ; private set ; }
