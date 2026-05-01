de6dad78a0dd7b07e483d90515987abbbc6576cb
bence0601 <nagybence0601@gmail.com>
2026-05-01 23:36:28 +0200

    feat(backend): implement GetAllUsers function

---
+++ HaccpBackend/Features/User/GetAllUsers.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using HaccpBackend.Data;
+using HaccpBackend.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static HaccpBackend.Features.User._Shared;
+
+namespace HaccpBackend.Features.User
+{
+    public static class GetAllUsers
+    {
+        public sealed record Query : IRequest<List<UserResponse>>;
+
+        internal sealed class Handler : IRequestHandler<Query, List<UserResponse>>
+        {
+            private readonly AppDataContext _appDataContext;
+
+            public Handler(AppDataContext appDataContext)
+            {
+                _appDataContext = appDataContext;
+            }
+
+            public async Task<List<UserResponse>> Handle(
+                Query request,
+                CancellationToken cancellationToken
+            )
+            {
+                return await _appDataContext
+                    .Users.AsNoTracking()
+                    .Select(u => new UserResponse(u.Id!, u.UserName!, u.Email!, u.Organization.Id))
+                    .ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ HaccpBackend/Features/User/_Shared.cs
@@ -0,0 +1,14 @@
+﻿using HaccpBackend.Domain.Entities;
+
+namespace HaccpBackend.Features.User
+{
+    public static class _Shared
+    {
+        public sealed record UserResponse(
+            int UserID,
+            string UserName,
+            string Email,
+            int OrganizationID
+        );
+    }
+}
